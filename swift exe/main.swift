51f9bb19f2af934f5e893e267d1224fa6d8275bd
20181102932 <908907990@qq.com>
2019-09-25 10:50:03 +0800

    Initial Commit

---
+++ swift exe/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift exe
+//
+//  Created by s20181102932 on 2019/9/25.
+//  Copyright © 2019 wjx. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
